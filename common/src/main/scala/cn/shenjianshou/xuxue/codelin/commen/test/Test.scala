7aa69cb25d93045fb80b1144294028781e69881f
xuxue <xujiqiang@bihu.mobi>
2016-12-20 20:07:35 +0800

    Initial commit

---
+++ common/src/main/scala/cn/shenjianshou/xuxue/codelin/commen/test/Test.scala
@@ -0,0 +1,10 @@
+package cn.shenjianshou.xuxue.codelin.commen.test
+
+object Test{
+  def main(args: Array[String]): Unit = {
+    println("Hello")
+  }
+  def hello():Unit={
+    println("Hello")
+  }
+}
